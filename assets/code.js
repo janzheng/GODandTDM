da078c742afa0d47114b344222761923aa15e794
Jan Zheng <janeazy@gmail.com>
2013-10-24 16:45:14 -0400

    post-launch commit 1
    added demographics ga.js code
    testing event code; moved JS to main html file
    new zip file

--- assets/code.js
+++ assets/code.js
@@ -2,38 +2,6 @@
 $( document ).ready(function() {
   // Handler for .ready() called.
 
-  // $('.main-title').hover(
-  $('#page-intro').hover(
-    function() {
-      $('.wrapper').animate({
-          backgroundColor: jQuery.Color( "rgba(0,0,0,0.7)" )
-      }, 500 );
-    },
-    function() {
-      $('.wrapper').animate({
-          backgroundColor: jQuery.Color( "rgba(0,0,0,1)" )
-      }, 500 );
-    }
-  );
-
-
-
-
-
-  // analytics
-  $('#download').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'download');
-  });
-  $('#facebook').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'facebook');
-  });
-  $('#twitter').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'twitter');
-  });
-  $('#youtube').on('click', function() {
-    ga('send', 'event', 'button', 'click', 'youtube');
-  });
-
 
 });
 
